515977db667dcb9ba25be83b3c3a44e0a8846704
Luisokl <luis.melo.okl@outlook.com>
2024-08-07 21:35:45 -0300

    'configurando'

---
+++ src/redux/user/slice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    user: {
+        id: 1,
+        name: 'Luis Fernando',
+        email: 'teste@luis.com',
+        address: null,
+    }
+}
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+
+    }
+})
+
+export default userSlice.reducer;
